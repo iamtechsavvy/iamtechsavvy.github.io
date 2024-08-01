f73dc52f0f7533e3f06379fede763df1b3057a46
naresh.vasikarla <naresh.vasikarla@gmail.com>
2024-07-31 22:39:35 -0500

    changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configuration options here
+  basePath: '/iamtechsavvy.github.io',  // Replace with your repo name
+  assetPrefix: '/iamtechsavvy.github.io/',
+  trailingSlash: true, // Ensure trailing slashes for static HTML export
+  output: 'export',
 }
 
 export default nextConfig;
